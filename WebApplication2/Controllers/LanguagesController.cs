ebe67b4bd1fe492dbbdd8d92d7ba44188ee28c52
isabelvalente <ivalente@spartaglobal.com>
2018-08-08 18:40:40 +0100

    initial commit, most routes set up

---
+++ WebApplication2/Controllers/LanguagesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        // GET: Languages
+        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Languages are the spice of life!";
+
+            return View();
+        }
+
+        [Route("Languages/Ruby")]
+        public ActionResult Ruby()
+        {
+            ViewBag.Message = "Ruby is the language of the people!";
+
+            return View();
+        }
+
+        [Route("Languages/C#")]
+        public ActionResult CSharp()
+        {
+            ViewBag.Message = "C Sharp can be tricky.";
+
+            return View();
+        }
+
+        [Route("Languages/Javascript")]
+        public ActionResult Javascript()
+        {
+            ViewBag.Message = "Build powerful games with Javascript.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ProjectController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Project> projects;
+
+        public ProjectController()
+        {
+            projects = cache["projects"] as List<Project>;
+            if (projects == null)
+            {
+                projects = new List<Project>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["projects"] = projects;
+        }
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewProject(string id)
+        {
+            Project project = projects.FirstOrDefault(c => c.Id == id);
+            if(project == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(project);
+            }
+
+            
+        }
+
+        public ActionResult AddProject()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProject(Project project)
+        {
+            project.Id = Guid.NewGuid().ToString();
+            projects.Add(project);
+            SaveCache();
+
+            return RedirectToAction("ProjectList");
+        }
+
+        public ActionResult EditProject(string id)
+        {
+            Project project = projects.FirstOrDefault(c => c.Id == id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(project);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult EditProject(Project project, string id)
+        {
+            var projectToEdit = projects.FirstOrDefault(c => c.Id == id);
+
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                projectToEdit.Name = project.Name;
+                projectToEdit.Description = project.Description;
+                projectToEdit.Language = project.Language;
+                SaveCache();
+
+                return RedirectToAction("ProjectList");
+            }
+        }
+
+        public ActionResult ProjectList()
+        {
+             return View(projects);
+        }
+    }
+
+
+}
