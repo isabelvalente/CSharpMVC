c17afd926c8850e462b5e993e805316470a1b280
HZNFSheikh <hsheikh@spartaglobal.com>
2018-08-09 17:02:19 +0100

    Edited description label and field, added drop down field for languages.

--- WebApplication2/Models/Project.cs
+++ WebApplication2/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,8 @@ namespace WebApplication2.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        [DisplayName ("Repository Link")]
+        public string RepositoryLink { get; set; }
         public string Language { get; set; }
 
     }
