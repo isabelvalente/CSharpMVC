fcae87a25b20b5377ad3264545b4382cb413241d
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-08-09 16:50:38 +0100

    Finished styling for home and projects index pages

--- WebApplication2/Models/Project.cs
+++ WebApplication2/Models/Project.cs
@@ -9,7 +9,7 @@ namespace WebApplication2.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Description Hello { get; set; }
         public string Language { get; set; }
 
     }
